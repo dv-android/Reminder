3e17fc6510f3d2bf826e780f7063c2fc181b31d3
dv-android <devang.droid@gmail.com>
2017-07-19 21:15:01 +0530

    BirhdayDialog-Save Button-Update Fragment functionality implemnted

--- app/src/main/java/com/hfad/reminder/ReminderDatabaseHelper.java
+++ app/src/main/java/com/hfad/reminder/ReminderDatabaseHelper.java
@@ -13,34 +13,37 @@ import android.util.Log;
 public class ReminderDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "remindroid";
-    private static final int DB_VERSION = 1;
-
+    private static final int DB_VERSION = 3;
+    public static final String TABLE_BDAY = "BIRTHDAY";
+    public SQLiteDatabase dbb;
 
     ReminderDatabaseHelper(Context context){
         super(context,DB_NAME,null,DB_VERSION);
+        dbb = getReadableDatabase();
         Log.d("Database","DB helper created");
     }
 
     public void onCreate(SQLiteDatabase db){
 
-        db.execSQL("CREATE TABLE REMINDER (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+        db.execSQL("CREATE TABLE BIRTHDAY (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "FIRST_NAME TEXT,"
                 +" LAST_NAME TEXT,"
                 +"DATE_TIME LONG);");
         Log.d("Database","DB created");
+        Log.d("Check","check db created");
     }
 
     public static void insertBdayDetails(SQLiteDatabase db,String firstName,
                                     String lastName,long dateTime){
-        ContentValues remindValues = new ContentValues();
-        remindValues.put("FIRST_NAME",firstName);
-        remindValues.put("LAST_NAME",lastName);
-        remindValues.put("DATE_TIME",dateTime);
-        db.insert("REMINDER",null,remindValues);
+        ContentValues bDayValues = new ContentValues();
+        bDayValues.put("FIRST_NAME",firstName);
+        bDayValues.put("LAST_NAME",lastName);
+        bDayValues.put("DATE_TIME",dateTime);
+        db.insert("BIRTHDAY",null,bDayValues);
         Log.d("Database ","Inserted"+firstName+lastName+dateTime);
     }
     public void onUpgrade(SQLiteDatabase db,int oldVersion,int neewVersion){
-
+        Log.d("Check","Upgrade called");
     }
 
 
